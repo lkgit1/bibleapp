56d699763f531f7868f74f0ee5841d9cb02f9775
Daniel <olalekan.omoniyi98@gmail.com>
2023-12-15 07:31:15 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+<ion-page>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title style='text-align: center;'>Select Testament</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        <ion-grid>
+            <ion-row>
+                <ion-col class='testament-btns'>
+                    <ion-button router-link="/new" router-direction="forward" class='btn' expand="block" size='large'>New Testament</ion-button>
+                    <ion-button router-link="/old" router-direction="forward" class='btn' expand="block" size='large'>Old Testament</ion-button>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+    </ion-content>
+</ion-page>
+</template>
+
+<style>
+.testament-btns {
+    margin-top: 50%;
+}
+
+.btn {
+    margin-bottom: 30px;
+}
+</style>
